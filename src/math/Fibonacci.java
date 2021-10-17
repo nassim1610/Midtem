195abfc968e9a934f27e9905155a89ea4e6a78ff
Nassim Amiri <nassimamiri29@gmail.com>
2021-10-17 18:06:57 -0400

    fibonacci

--- src/math/Fibonacci.java
+++ src/math/Fibonacci.java
@@ -3,11 +3,19 @@ package math;
 import java.util.Arrays;
 
 public class Fibonacci {
-    public static void main(String[] args) {
+
          /*
           Write 40 Fibonacci numbers with java.
          */
 
+        public static void main(String[] args) {
+            int prev = 0;
+            int next = 1;
 
+            for (int i = 0; i < 40; i++) {
+                System.out.println(prev);
+                prev = next - prev;
+                next = next +prev;
+            }
     }
 }
